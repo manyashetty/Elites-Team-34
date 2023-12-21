ca63eb6034cc3d8a27b3b51284debc610882904a
manyashetty <m12shetty@gmail.com>
2023-12-21 14:41:25 +0530

    third commit

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,9 +3,9 @@ const userController = require("../controllers/userController")
 const projectController = require("../controllers/projectController")
 const {authenticateMiddleware} = require("../controllers/projectController")
 const contactController = require("../controllers/conactController")
-// const feedController = require("../controllers/feedController")
+
 const servicesController = require("../controllers/servicesController")
-const testimonialController = require("../controllers/testimonialController")
+
 const {upload} = require("../middleware/multer");
 const {getAllMessages , postNewMessage} = require('../controllers/messageController');
 const router=express.Router()
@@ -29,12 +29,6 @@ router.put("/projects",upload.single('project_image'),projectController.updatePr
 router.post("/contacts",authenticateMiddleware,contactController.submitContactForm)
 router.get("/contacts",authenticateMiddleware,contactController.displayForm) 
 
-// FEEDS CREATION, DISPLAY, DELETE, UPDATE ROUTE
-// router.post("/feeds",authenticateMiddleware,upload.single('image'),feedController.createFeeds)
-// router.get("/feeds", authenticateMiddleware,feedController.getFeeds)
-// router.delete("/feeds",feedController.deleteFeed)
-// router.put("/feeds",upload.single('image'),feedController.updateFeed)
-
 
 // SERVICES CREATION, DISPLAY, DELETE, UPDATE ROUTE
 router.post('/services', authenticateMiddleware,upload.single('service_image'), servicesController.createService);
@@ -43,12 +37,6 @@ router.delete('/services', servicesController.deleteService);
 router.put('/services',upload.single('service_image'), servicesController.updateService)
 
 
-
-// TESTIMONIALS CREATION, DISPLAY, DELETE, UPDATE ROUTE
-router.post('/testimonial',authenticateMiddleware,upload.single('client_image'),testimonialController.createTestimonial );
-router.get('/testimonial', authenticateMiddleware,testimonialController.getTestimonials);
-router.delete('/testimonial', testimonialController.deleteTestimonial);
-router.put('/testimonial',upload.single('client_image'), testimonialController.updateTestimonial);
 
 
 // MESSAGES
